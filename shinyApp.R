e50b83c58738c0eab671d04a1035698669cbebf8
Ahmad Abdel-Azim <agabdel.azim@gmail.com>
2018-12-18 19:48:02 -0600

    Changed themes in shiny and fixed prediction equations.

--- shinyApp.R
+++ shinyApp.R
@@ -6,7 +6,7 @@ library(shinythemes)
 
 
 ui <- navbarPage(
-  theme = shinytheme("cerulean"),
+  theme = shinytheme("sandstone"),
   "Effect of Bacterial Genotype on Growth Levels Over Multiple Exposures",
   tabPanel("Simulation",
   fluidPage(
@@ -41,19 +41,19 @@ ui <- navbarPage(
              fluidPage(
                sidebarLayout(
                  sidebarPanel(
-                   numericInput("Nl2", "Number of loci in second section",
+                   numericInput("Nl2.O", "Number of loci in second section",
                                 value = 6),
-                   numericInput("Nl3", "Number of loci in third section",
+                   numericInput("Nl3.O", "Number of loci in third section",
                                 value = 10),
-                   numericInput("Ng1", "Number of genes in first-section loci",
+                   numericInput("Ng1.O", "Number of genes in first-section loci",
                                 value = 2),
-                   numericInput("Ng3", "Number of genes in third-section loci",
+                   numericInput("Ng3.O", "Number of genes in third-section loci",
                                 value = 12),
-                   sliderInput("days", "Length of Simulation, in Days",
+                   sliderInput("days.O", "Length of Simulation, in Days",
                                min = 2,
                                max = 40,
                                value = 30),
-                   sliderInput("ylim", label = "y-axis range",
+                   sliderInput("ylim.O", label = "y-axis range",
                                min = 0,
                                max = 3,
                                value = c(0, 1.2),
@@ -68,19 +68,19 @@ ui <- navbarPage(
              fluidPage(
                sidebarLayout(
                  sidebarPanel(
-                   numericInput("Nl2", "Number of loci in second section",
+                   numericInput("Nl2.1", "Number of loci in second section",
                                 value = 6),
-                   numericInput("Nl3", "Number of loci in third section",
+                   numericInput("Nl3.1", "Number of loci in third section",
                                 value = 10),
-                   numericInput("Ng1", "Number of genes in first-section loci",
+                   numericInput("Ng1.1", "Number of genes in first-section loci",
                                 value = 2),
-                   numericInput("Ng3", "Number of genes in third-section loci",
+                   numericInput("Ng3.1", "Number of genes in third-section loci",
                                 value = 12),
-                   sliderInput("days", "Length of Simulation, in Days",
+                   sliderInput("days.1", "Length of Simulation, in Days",
                                min = 2,
                                max = 40,
                                value = 30),
-                   sliderInput("ylim", label = "y-axis range",
+                   sliderInput("ylim.1", label = "y-axis range",
                                min = 0,
                                max = 3,
                                value = c(0, 1.2),
@@ -95,19 +95,19 @@ ui <- navbarPage(
              fluidPage(
                sidebarLayout(
                  sidebarPanel(
-                   numericInput("Nl2", "Number of loci in second section",
+                   numericInput("Nl2.2", "Number of loci in second section",
                                 value = 6),
-                   numericInput("Nl3", "Number of loci in third section",
+                   numericInput("Nl3.2", "Number of loci in third section",
                                 value = 10),
-                   numericInput("Ng1", "Number of genes in first-section loci",
+                   numericInput("Ng1.2", "Number of genes in first-section loci",
                                 value = 2),
-                   numericInput("Ng3", "Number of genes in third-section loci",
+                   numericInput("Ng3.2", "Number of genes in third-section loci",
                                 value = 12),
-                   sliderInput("days", "Length of Simulation, in Days",
+                   sliderInput("days.2", "Length of Simulation, in Days",
                                min = 2,
                                max = 40,
                                value = 30),
-                   sliderInput("ylim", label = "y-axis range",
+                   sliderInput("ylim.2", label = "y-axis range",
                                min = 0,
                                max = 3,
                                value = c(0, 1.2),
@@ -122,19 +122,19 @@ ui <- navbarPage(
              fluidPage(
                sidebarLayout(
                  sidebarPanel(
-                   numericInput("Nl2", "Number of loci in second section",
+                   numericInput("Nl2.3", "Number of loci in second section",
                                 value = 6),
-                   numericInput("Nl3", "Number of loci in third section",
+                   numericInput("Nl3.3", "Number of loci in third section",
                                 value = 10),
-                   numericInput("Ng1", "Number of genes in first-section loci",
+                   numericInput("Ng1.3", "Number of genes in first-section loci",
                                 value = 2),
-                   numericInput("Ng3", "Number of genes in third-section loci",
+                   numericInput("Ng3.3", "Number of genes in third-section loci",
                                 value = 12),
-                   sliderInput("days", "Length of Simulation, in Days",
+                   sliderInput("days.3", "Length of Simulation, in Days",
                                min = 2,
                                max = 40,
                                value = 30),
-                   sliderInput("ylim", label = "y-axis range",
+                   sliderInput("ylim.3", label = "y-axis range",
                                min = 0,
                                max = 3,
                                value = c(0, 1.2),
@@ -152,7 +152,7 @@ ui <- navbarPage(
 server <- function(input, output) {
      
   d.lm <- reactive({
-    lm(fit ~ poly(days,3, raw = T) + Nl2 + Nl3 + Ng1 + Ng3, d)
+    lm(fit ~ poly(days,3, raw = T) + Nl2 + poly(Nl3, 2) + Ng1 + Ng3, d)
   })
   d.lm1 <- reactive({
     lm(fit.1 ~ poly(days,3, raw = T) + Nl2 + Nl3 + Ng1 + Ng3, d)
@@ -168,14 +168,17 @@ server <- function(input, output) {
   newD <- reactive({
     data.frame(days = 1:input$days, Nl2 = input$Nl2, Nl3 = input$Nl3, Ng1 = input$Ng1, Ng3 = input$Ng3)
     })
+  newDO <- reactive({
+    data.frame(days = 1:input$days.O, Nl2 = input$Nl2.O, Nl3 = input$Nl3.O, Ng1 = input$Ng1.O, Ng3 = input$Ng3.O)
+  })
   newD1 <- reactive({
-    data.frame(days = 1:input$days, Nl2 = input$Nl2, Nl3 = input$Nl3, Ng1 = input$Ng1, Ng3 = input$Ng3)
+    data.frame(days = 1:input$days.1, Nl2 = input$Nl2.1, Nl3 = input$Nl3.1, Ng1 = input$Ng1.1, Ng3 = input$Ng3.1)
   })
   newD2 <- reactive({
-    data.frame(days = 1:input$days, Nl2 = input$Nl2, Nl3 = input$Nl3, Ng1 = input$Ng1, Ng3 = input$Ng3)
+    data.frame(days = 1:input$days.2, Nl2 = input$Nl2.2, Nl3 = input$Nl3.2, Ng1 = input$Ng1.2, Ng3 = input$Ng3.2)
   })
   newD3 <- reactive({
-    data.frame(days = 1:input$days, Nl2 = input$Nl2, Nl3 = input$Nl3, Ng1 = input$Ng1, Ng3 = input$Ng3)
+    data.frame(days = 1:input$days.3, Nl2 = input$Nl2.3, Nl3 = input$Nl3.3, Ng1 = input$Ng1.3, Ng3 = input$Ng3.3)
   })
   
   
@@ -183,6 +186,9 @@ server <- function(input, output) {
   fit.pred <- reactive({
     predict(d.lm(), newdata = newD())
     })
+  fit.predO <- reactive({
+    predict(d.lm(), newdata = newDO())
+  })
   fit.pred1 <- reactive({
     predict(d.lm1(), newdata = newD1())
   })
@@ -195,22 +201,22 @@ server <- function(input, output) {
   
   
   output$overall <- renderPlot({
-    plot(1:input$days, fit.pred(), xlim = c(1, input$days), ylim = input$ylim, col='dark green', pch=16, cex = 2, cex.lab = 1.7, cex.axis = 1.5, 
+    plot(1:input$days.O, fit.predO(), xlim = c(1, input$days.O), ylim = input$ylim.O, col='dark green', pch=16, cex = 2, cex.lab = 1.7, cex.axis = 1.5, 
          ylab = 'Overall Fitness', xlab = 'Days', type = "b")
   })
   
   output$sect1 <- renderPlot({
-    plot(1:input$days, fit.pred1(), xlim = c(1, input$days), ylim = input$ylim, col='dark green', pch=16, cex = 2, cex.lab = 1.7, cex.axis = 1.5, 
+    plot(1:input$days.1, fit.pred1(), xlim = c(1, input$days.1), ylim = input$ylim.1, col='red', pch=16, cex = 2, cex.lab = 1.7, cex.axis = 1.5, 
          ylab = 'Section 1 Fitness', xlab = 'Days', type = "b")
   })
   
   output$sect2 <- renderPlot({
-    plot(1:input$days, fit.pred2(), xlim = c(1, input$days), ylim = input$ylim, col='dark green', pch=16, cex = 2, cex.lab = 1.7, cex.axis = 1.5, 
+    plot(1:input$days.2, fit.pred2(), xlim = c(1, input$days.2), ylim = input$ylim.2, col='dark orange', pch=16, cex = 2, cex.lab = 1.7, cex.axis = 1.5, 
          ylab = 'Section 2 Fitness', xlab = 'Days', type = "b")
   })
   
   output$sect3 <- renderPlot({
-    plot(1:input$days, fit.pred3(), xlim = c(1, input$days), ylim = input$ylim, col='dark green', pch=16, cex = 2, cex.lab = 1.7, cex.axis = 1.5, 
+    plot(1:input$days.3, fit.pred3(), xlim = c(1, input$days.3), ylim = input$ylim.3, col='yellow', pch=16, cex = 2, cex.lab = 1.7, cex.axis = 1.5, 
          ylab = 'Section 3 Fitness', xlab = 'Days', type = "b")
   })
   
